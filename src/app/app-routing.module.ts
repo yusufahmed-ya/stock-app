7e5b0ff6912de851fbf4845105807c9e0b0fefcf
Yusuf Ahmed <yusufahmed6211@gmail.com>
2020-04-10 15:49:36 +0100

    added card with dropdown and made home page the default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'home',
     component: HomeComponent
   }
